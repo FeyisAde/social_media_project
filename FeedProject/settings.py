a49b238171cf37b2e07c09de7b53f83e8cb9288c
FeyisAde <256893395+FeyisAde@users.noreply.github.com>
2026-05-03 21:22:25 -0500

    updated settings

--- FeedProject/settings.py
+++ FeedProject/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['feed-56d08a3382f3.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
